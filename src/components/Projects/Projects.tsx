946d45ff2ded379748dae231ce929df56e16fc90
maddiemasie <maddiemasiello@gmail.com>
2025-04-02 12:34:07 -0700

    removed '

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -44,7 +44,7 @@ const projects: ProjectEntry[] = [
        My group and I designed a custom interface for our designed circuits. Throughout the quarter, 
        we analyzed, designed, and constructed digital logic gates from, focusing on  Positive Emitter-Coupled 
        Logic (PECL), Transistor-Transistor Logic (TTL), and Complementary Metal-Oxide-Semiconductor (CMOS) technologies.
-       The project involved building a PECL OR-NOR gate, interfacing TTL to PECL, and further integrating the PECL with the CMOS.' 
+       The project involved building a PECL OR-NOR gate, interfacing TTL to PECL, and further integrating the PECL with the CMOS. 
       </>
     ),
 
