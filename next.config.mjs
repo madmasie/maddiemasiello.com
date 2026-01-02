1933a4d4e03004fc3ad3d7a1aeaa8e5783ef27e9
Chris Lawson <git@chrislawson.dev>
2026-01-01 18:43:28 -0800

    feat: migrate to static next

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+// @ts-check
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  output: "export",
+};
 
 export default nextConfig;
